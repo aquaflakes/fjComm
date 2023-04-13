c464cb833aa13bed5b86eb0d58927af32d16d323
fjzhu <fjzhu@fafu.edu.cn>
2023-04-13 13:59:49 +0800

    202304_CN_sources

--- R/R_utility.R
+++ R/R_utility.R
@@ -6,3 +6,8 @@ largeListToDf <- function(list)
 
 letter2num <- function(x) { sapply(x, function(x) {utf8ToInt(x) - utf8ToInt("A") + 1L}) }
 num2letter <- function(x) {  sapply(x, function(x) {intToUtf8 (x + utf8ToInt("A") - 1L)})  }
+
+CN_sources <- function(){
+  options("repos"=c(CRAN="https://mirrors.tuna.tsinghua.edu.cn/CRAN/"))
+  options(BioC_mirror="https://mirrors.tuna.tsinghua.edu.cn/bioconductor")
+}
