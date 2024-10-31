7be72f1aff2fb6dc3d75b3442a293a16874eb88c
fjzhu <fjzhu@fafu.edu.cn>
2024-10-31 10:21:42 +0800

    "20241031 mclapply"

--- R/2017_04_20_parallel.R
+++ R/2017_04_20_parallel.R
@@ -17,13 +17,28 @@ parallel_cmd_gen<-function(cmd="samtools view -c {}",params=c("file1","file2","f
   glue::glue("parallel -j {threads} -k {cmd} ::: ") %>%paste0(paste0(params,collapse = " "))
 
 
-mclapply<-function(params,FUN,mc.cores=2,loopMax=10,curr_loop=1,...){
+# mclapply<-function(params,FUN,mc.cores=2,loopMax=10,curr_loop=1,...){
+#   # robust parall lapply, will try multiple times
+#   options(warn = 0)
+#   print("mclapply loop: {curr_loop}" %>% glue::glue())
+#   result=parallel::mclapply(params,FUN,mc.cores = mc.cores,...)
+#   if(curr_loop>=loopMax) {return(result)} else {curr_loop=curr_loop+1}
+#   failed_Ind= map(result,~.x[1] %>% stringr::str_detect("Error")) %>% unlist() %>% which
+#   if(any(failed_Ind)){
+#     result_= mclapply(params[failed_Ind],FUN,mc.cores = mc.cores,loopMax=loopMax,curr_loop=curr_loop,...)
+#     for (Ind in seq_along(result_)) {result[[failed_Ind[Ind]]]=result_[[Ind]]}
+#   }
+#   return(result)
+# }
+
+## 2024-10-31
+mclapply<-function(params,FUN,mc.cores=2,loopMax=4,curr_loop=1,...){
   # robust parall lapply, will try multiple times
   options(warn = 0)
   print("mclapply loop: {curr_loop}" %>% glue::glue())
   result=parallel::mclapply(params,FUN,mc.cores = mc.cores,...)
   if(curr_loop>=loopMax) {return(result)} else {curr_loop=curr_loop+1}
-  failed_Ind= map(result,~.x[1] %>% stringr::str_detect("Error")) %>% unlist() %>% which
+  failed_Ind= map(result,~is.character(.x[1]) && (.x[1] %>% stringr::str_detect("Error")) ) %>% unlist() %>% which
   if(any(failed_Ind)){
     result_= mclapply(params[failed_Ind],FUN,mc.cores = mc.cores,loopMax=loopMax,curr_loop=curr_loop,...)
     for (Ind in seq_along(result_)) {result[[failed_Ind[Ind]]]=result_[[Ind]]}
